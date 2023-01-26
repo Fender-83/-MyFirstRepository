7e6ac92bee99b4892897b62c741d24329289507e
Fender-83 <fenderpugach@gmail.com>
2023-01-26 15:40:46 +0700

    Заготовка вопросов к проекту

--- 5.6. Итоговый проект/Program.cs
+++ 5.6. Итоговый проект/Program.cs
@@ -11,6 +11,21 @@ namespace _5._6.Итоговый_проект
         static void Main(string[] args)
         {
             Console.WriteLine("Задание 5.6. \nИтоговый проект.");
+            Console.WriteLine();
+
+            Console.WriteLine("Введите имя");
+            Console.WriteLine("Введите фамилию");
+            Console.WriteLine("Введите возраст");
+
+            Console.WriteLine("Есть ли у Вас питомец?");
+            Console.WriteLine("Сколько питомцев у Вас?");
+            Console.WriteLine("Какая у него(нее) кличка?");
+
+            Console.WriteLine("Сколько у Вас любимых цветов?");
+
+
+
+
         }
     }
 }
