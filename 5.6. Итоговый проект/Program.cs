a36d438af336cd52355aa9a20d1b347d2d910752
Fender-83 <fenderpugach@gmail.com>
2023-01-26 15:34:53 +0700

    Создан Итоговый проект

---
+++ 5.6. Итоговый проект/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._6.Итоговый_проект
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 5.6. \nИтоговый проект.");
+        }
+    }
+}
