c082875b6eae735073431b88f7137ad84fdb0a78
Fender-83 <fenderpugach@gmail.com>
2023-01-27 00:28:47 +0700

    Подготовка Итогового проекта

--- 5.6. Итоговый проект/Program.cs
+++ 5.6. Итоговый проект/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,21 +11,33 @@ namespace _5._6.Итоговый_проект
     internal class Program
     {
         static void Main(string[] args)
+
+        //Необходимо создать метод, который заполняет данные с клавиатуры по пользователю(возвращает кортеж) :
+        //        Имя;
+        //        Фамилия;
+        //        Возраст;
+        //        Наличие питомца;
+        //Если питомец есть, то запросить количество питомцев;
+        //Если питомец есть, вызвать метод, принимающий на вход количество питомцев и возвращающий массив их кличек(заполнение с клавиатуры);
+        //Запросить количество любимых цветов;
+        //Вызвать метод, который возвращает массив любимых цветов по их количеству(заполнение с клавиатуры);
+        //Сделать проверку, ввёл ли пользователь корректные числа: возраст, количество питомцев, количество цветов в отдельном методе;
+        //Требуется проверка корректного ввода значений и повтор ввода, если ввод некорректен;
+        //Корректный ввод: ввод числа типа int больше 0.
+        //Метод, который принимает кортеж из предыдущего шага и показывает на экран данные.
+        //Вызов методов из метода Main.
         {
             Console.WriteLine("Задание 5.6. \nИтоговый проект.");
             Console.WriteLine();
 
-            Console.WriteLine("Введите имя");
-            Console.WriteLine("Введите фамилию");
-            Console.WriteLine("Введите возраст");
+            (string name, string lastName, int age, string[] pets, string[] colors) userDate = ProjectMethods.GetUserData();
 
-            Console.WriteLine("Есть ли у Вас питомец?");
-            Console.WriteLine("Сколько питомцев у Вас?");
-            Console.WriteLine("Какая у него(нее) кличка?");
+            Console.Clear();
+            Console.WriteLine("\n\n");
 
-            Console.WriteLine("Сколько у Вас любимых цветов?");
+            ProjectMethods.PrintUserData(userDate);
 
-
+            Console.ReadKey();
 
 
         }
---
+++ 5.6. Итоговый проект/ProjectMethods.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._6.Итоговый_проект
+{
+    public class ProjectMethods
+    {
+
+        public static (string name, string lastName, int age, string[] pets, string[] colors) GetUserData()
+        {
+            (string name, string lastName, int age, string[] pets, string[] colors) user;
+
+            Console.WriteLine("Введите имя");
+            user.name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию");
+            user.lastName = Console.ReadLine();
+
+            Console.WriteLine("Введите возраст");
+            user.age = GetIntNumber();
+
+            Console.WriteLine("Есть ли у Вас питомец?");
+            bool havePet = GetBool();
+            int numberOfPets = 0;
+
+            if (havePet)
+            {
+                Console.WriteLine("Напишите количество питомцев");
+                numberOfPets = GetIntNumber();
+                user.pets = GetPetsNames(numberOfPets);
+            }
+            else
+            {
+                user.pets = new string[0];
+            }
+
+            Console.WriteLine("Сколько у Вас любимых цветов?");
+            int numberOfColor = GetIntNumber();
+            user.colors = GetColorsNames(numberOfColor);
+
+
+            return user;
+        }
+
+        public static int GetIntNumber()
+        {
+            int number;
+            bool isNumberCorrect;
+            isNumberCorrect = int.TryParse(Console.ReadLine(), out number);
+            while (!isNumberCorrect || number < 1)
+            {
+                Console.WriteLine("Напишите, пожалуйста, верное значение");
+                isNumberCorrect = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+
+        public static string[] GetPetsNames(int numberOfPets)
+        {
+            string[] names = new string[numberOfPets];
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"Напишите имя питомца {i+1}");
+                names[i] = Console.ReadLine();
+            }
+            return names;
+        }
+
+        public static string[] GetColorsNames(int numberOfColors)
+        {
+            string[] colors = new string[numberOfColors];
+            for (int i = 0; i < colors.Length; i++)
+            {
+                Console.WriteLine($"Напишите название цвета  {i+1}");
+                colors[i] = Console.ReadLine();
+            }
+            return colors;
+        }
+
+        public static bool GetBool()
+        {
+            bool respons;
+            bool isNumberCorrect = bool.TryParse(Console.ReadLine(), out respons);
+            while (!isNumberCorrect)
+            {
+                Console.WriteLine("Укажите \"true\" or \"false\":");
+                isNumberCorrect = bool.TryParse(Console.ReadLine(), out respons);
+            }
+            return respons;
+        }
+
+        public static void PrintUserData((string name, string lastName, int age, string[] pets, string[] colors) user)
+        {
+            Console.WriteLine($"Name: {user.name}");
+            Console.WriteLine($"Lastname: {user.lastName}");
+            Console.WriteLine($"Age: {user.age}");
+            if (user.pets.Length > 0)
+            {
+                for (int i = 0; i < user.pets.Length; i++)
+                {
+                    Console.WriteLine(user.pets[i]);
+                }
+            }
+
+            if (user.colors.Length > 0)
+            {
+                foreach (var color in user.colors)
+                {
+                    Console.WriteLine(color);
+                }
+            }
+        }
+
+
+    }
+}
